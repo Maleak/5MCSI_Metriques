1a58c3791f89d507c431bb4c25f2b3ab84925ffc
Aboubacar Malek CAMARA <36192328+Maleak@users.noreply.github.com>
2025-04-17 12:27:51 +0200

    Update __init__.py
    reverse

--- __init__.py
+++ __init__.py
@@ -8,6 +8,8 @@ import sqlite3
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
+def hello_world():
+    return render_template('hello.html')
 
 @app.route("/contact/")
 def MaPremiereAPI():
@@ -38,8 +40,7 @@ def CommitsGithub():
     return render_template("commits.html")
 
 
-def hello_world():
-    return render_template('hello.html')
+
   
 if __name__ == "__main__":
   app.run(debug=True)
